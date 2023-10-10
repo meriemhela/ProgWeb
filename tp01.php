4b1ceff6a44560eb1fb50d9f47590a5ab22f118c
Meriem Helali <95054117+meriemhela@users.noreply.github.com>
2023-10-10 21:30:29 +0200

    Add files via upload

---
+++ tp01.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<?php
+    
+    echo "hello ";
+?>
+<body>
+
+    <h1>tp 1</h1>
+    <form action="/action_page.php">
+      <label for="fname">First name:</label><br>
+      <input type="text" id="fname" name="fname" ><br>
+      <label for="lname">Last name:</label><br>
+      <input type="text" id="lname" name="lname" ><br><br>
+      <input type="submit" value="Submit">
+    </form>
+    <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <t
+    d>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+
+</body>
+</html>
